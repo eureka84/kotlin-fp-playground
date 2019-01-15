175a01aafb23afdffcdec4b2dc5f80e435c4b4be
eureka84 <angelosciarra@ymail.com>
2019-01-15 08:31:47 +0100

    minor

--- src/main/kotlin/com/eureka/ctfp/chapter2/Memoization.kt
+++ src/main/kotlin/com/eureka/ctfp/chapter2/Memoization.kt
@@ -3,12 +3,12 @@ package com.eureka.ctfp.chapter2
 typealias Function1<Input, Output> = (Input) -> Output
 
 fun <Input, Output> Function1<Input, Output>.memoize(): Function1<Input, Output> {
-    val self = this
+    val original = this
     val storage: MutableMap<Input, Output> = mutableMapOf()
     return { i ->
         val stored = storage[i]
         if (stored == null) {
-            val value = self(i)
+            val value = original(i)
             storage[i] = value
             value
         } else stored
