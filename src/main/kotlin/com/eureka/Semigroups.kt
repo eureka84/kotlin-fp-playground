8bbb17ae01c517504536e1848d3e7bc93d808bf5
eureka84 <angelosciarra@ymail.com>
2019-01-18 23:00:55 +0100

    experiments with semigroups

--- src/main/kotlin/com/eureka/Semigroups.kt
+++ src/main/kotlin/com/eureka/Semigroups.kt
@@ -3,27 +3,29 @@ package com.eureka
 import arrow.core.Predicate
 import arrow.typeclasses.Semigroup
 
-object Syntax {
 
-    fun Boolean.Companion.andSemiGroup(): Semigroup<Boolean> {
-        return object : Semigroup<Boolean> {
-            override fun Boolean.combine(b: Boolean): Boolean = this && b
-        }
+fun Boolean.Companion.andSemiGroup(): Semigroup<Boolean> {
+    return object : Semigroup<Boolean> {
+        override fun Boolean.combine(b: Boolean): Boolean = this && b
     }
+}
 
-    fun Boolean.Companion.orSemiGroup(): Semigroup<Boolean> {
-        return object : Semigroup<Boolean> {
-            override fun Boolean.combine(b: Boolean): Boolean = this || b
-        }
+fun Boolean.Companion.orSemiGroup(): Semigroup<Boolean> {
+    return object : Semigroup<Boolean> {
+        override fun Boolean.combine(b: Boolean): Boolean = this || b
     }
+}
 
-//    fun <T> Predicate<T>.semigroup(s: Semigroup<Boolean>): Semigroup<Predicate<T>> = object : Semigroup<Predicate<T>> {
-//        override fun Predicate<T>.combine(other: Predicate<T>): Predicate<T> = { t: T ->
-//            val originalPredicate = this
-//            s.run {
-//                originalPredicate(t).combine(other(t))
-//            }
-//        }
-//    }
-
+class PredicateK {
+    companion object {
+        fun <T> semigroup(s: Semigroup<Boolean>): Semigroup<Predicate<T>> =
+            object : Semigroup<Predicate<T>> {
+                override fun Predicate<T>.combine(other: Predicate<T>): Predicate<T> = { t: T ->
+                    val originalPredicate = this
+                    s.run {
+                        originalPredicate(t).combine(other(t))
+                    }
+                }
+            }
+    }
 }
--- src/test/kotlin/com/eureka/arrow/SemigroupsTest.kt
+++ src/test/kotlin/com/eureka/arrow/SemigroupsTest.kt
@@ -1,7 +1,11 @@
 package com.eureka.arrow
 
+import arrow.core.Predicate
 import arrow.instances.semigroup
-import com.eureka.Syntax
+import arrow.typeclasses.Semigroup
+import com.eureka.PredicateK
+import com.eureka.andSemiGroup
+import com.eureka.orSemiGroup
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import kotlin.test.assertFalse
@@ -18,21 +22,28 @@ class SemigroupsTest {
 
     @Test
     fun booleans() {
-        Syntax.run {
-            Boolean.andSemiGroup().run {
-                assertTrue(true.combine(true))
-                assertFalse(true.combine(false))
-                assertFalse(false.combine(true))
-                assertFalse(false.combine(false))
-            }
+        Boolean.andSemiGroup().run {
+            assertTrue(true.combine(true))
+            assertFalse(true.combine(false))
+            assertFalse(false.combine(true))
+            assertFalse(false.combine(false))
+        }
 
-            Boolean.orSemiGroup().run {
-                assertTrue(true.combine(true))
-                assertTrue(true.combine(false))
-                assertTrue(false.combine(true))
-                assertFalse(false.combine(false))
-            }
+        Boolean.orSemiGroup().run {
+            assertTrue(true.combine(true))
+            assertTrue(true.combine(false))
+            assertTrue(false.combine(true))
+            assertFalse(false.combine(false))
         }
     }
 
+    @Test
+    fun predicates() {
+        val isGreaterThan3: Predicate<Int> = { x: Int -> x > 3 }
+        val isEven: Predicate<Int> = { x: Int -> x % 2 == 0 }
+        val semigroup: Semigroup<Predicate<Int>> = PredicateK.semigroup(Boolean.andSemiGroup())
+        semigroup.run {
+            assertTrue(isGreaterThan3.combine(isEven)(4))
+        }
+    }
 }
